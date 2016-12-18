973f8af9499dddf52dec1a5fc69bfcb1439a2cf6
Patrick Nicholls <padawagon@gmail.com>
2016-12-18 22:00:58 +1300

    Written first functional test!

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,19 @@
+import unittest 
 from selenium import webdriver
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_starting_a_new_todo_list(self):
+        # Heron
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+
+
+if __name__ == "__main__":
+    unittest.main(warnings='ignore')
